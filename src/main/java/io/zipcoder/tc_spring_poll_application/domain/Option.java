9d9a439155156024e164a4e333ff9982b9bd81d7
Byung-chun Yoo <yoobc1@gmail.com>
2018-01-04 14:20:56 -0500

    Added Option entity

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -0,0 +1,34 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "OPTION_ID")
+    Long id;
+
+    @Column(name = "OPTION_VALUE")
+    String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
